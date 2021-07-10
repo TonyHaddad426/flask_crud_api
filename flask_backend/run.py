a940b51ebcc31ebbb432742d4368b62d69c8a775
Tony Haddad <tony.haddad426@gmail.com>
2021-07-10 11:40:12 -0400

    Adding Heroku required files for deployment

--- flask_backend/run.py
+++ flask_backend/run.py
@@ -1,8 +1,8 @@
-# from app import app
-# from db import db 
+from app import app
+from db import db 
 
-# db.init_app(app)
+db.init_app(app)
 
-# @app.before_first_request # before the first request runs, the below function is ran, which will automatically create tables
-# def create_tables(): 
-#     db.create_all()
+@app.before_first_request # before the first request runs, the below function is ran, which will automatically create tables
+def create_tables(): 
+    db.create_all()
