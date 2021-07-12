335280ef1b27984f88d45b02fa9799b19a8d0dc0
Tony Haddad <tony.haddad426@gmail.com>
2021-07-12 17:01:27 -0400

    Added PostgresSQL environment variable and psycopg2

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -1,4 +1,4 @@
-
+import os 
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -19,7 +19,7 @@ from resources.store import Store, StoreList
 #jsonify is a method to convert dicts into JSON
 
 app = Flask(__name__) # create Flask object 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') # asks the OS for the defined environment variable
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # turns off flask sqlalchemy modification tracker 
 app.secret_key = 'tony'
 api = Api(app) 
